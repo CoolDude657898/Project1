b20fe2666cc83a14445b2326e2d8c56be85f8a02
CoolDude657898 <mjm4739@gmail.com>
2025-03-01 10:09:42 -0500

    Feat: Added "for" attribute to all labels in fieldset

--- project1starter.php
+++ project1starter.php
@@ -15,31 +15,31 @@
         <fieldset>
             <legend>What age are you? </legend>
             <input type="radio" name="age" id="0-12">
-            <label>0-12 </label>
+            <label for="0-12">0-12 </label>
             <input type="radio" name="age" id="13-17">
-            <label>13-17 </label>
+            <label for="13-17">13-17 </label>
             <input type="radio" name="age" id="18-22">
-            <label>18-22 </label>
+            <label for="18-22">18-22 </label>
             <input type="radio" name="age" id="23-27">
-            <label>23-27 </label>
+            <label for="23-27">23-27 </label>
             <input type="radio" name="age" id="28-32">
-            <label>28-32 </label>
+            <label for="28-32">28-32 </label>
             <input type="radio" name="age" id="33-37">
-            <label>33-37 </label>
+            <label for="33-37">33-37 </label>
             <input type="radio" name="age" id="38-42">
-            <label>38-42 </label>
+            <label for="38-42">38-42 </label>
             <input type="radio" name="age" id="43-47">
-            <label>43-47 </label>
+            <label for="43-47">43-47 </label>
             <input type="radio" name="age" id="48-52">
-            <label>48-52 </label>
+            <label for="48-52">48-52 </label>
             <input type="radio" name="age" id="53-57">
-            <label>53-57 </label>
+            <label for="53-57">53-57 </label>
             <input type="radio" name="age" id="58-62">
-            <label>58-62 </label>
+            <label for="58-62">58-62 </label>
             <input type="radio" name="age" id="63-67">
-            <label>63-67 </label>
+            <label for="63-67">63-67 </label>
             <input type="radio" name="age" id="68+">
-            <label>68+ </label>
+            <label for="68+">68+ </label>
         </fieldset>
 
         <select name="gender" id="gender">
