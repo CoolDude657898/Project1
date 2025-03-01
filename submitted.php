d5a9a51ac0c8b3b222b7090d6fb560aa4251ebc5
CoolDude657898 <mjm4739@gmail.com>
2025-03-01 15:28:02 -0500

    Feat: Added html boilerplate and paragraph to submitted page

---
+++ submitted.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Submitted</title>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <p>Your survey has been submitted! Thank you for taking time to take this survey. We value your feedback!</p>
+    </body>
+</html>
