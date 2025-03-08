34350f579deb21401822de8110598238ce1208a2
CoolDude657898 <mjm4739@gmail.com>
2025-03-08 13:08:57 -0500

    Feat: Added login to dbconfig

--- dbconfig.php
+++ dbconfig.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 function connectDB(){
-    $database = "your username";
-    $user = "your username";
-    $pass = "your password";
+    $database = "mmeacha5";
+    $user = "mmeacha5";
+    $pass = "customize";
     $host = "localhost";
     try {
         $db = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
